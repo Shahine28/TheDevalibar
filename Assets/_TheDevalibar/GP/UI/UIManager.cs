9f9a7bbaf00913165043e48bca48f6a0f8923e50
Enguerrand <enguerrand.cousin@gmail.com>
2025-05-15 18:28:10 +0200

    [ADD]: Text settings menu
    mod:
    - scene
    - UIManager

--- Assets/_TheDevalibar/GP/UI/UIManager.cs
+++ Assets/_TheDevalibar/GP/UI/UIManager.cs
@@ -8,19 +8,18 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject _settingsMenus;
     [SerializeField] GameObject _rebindMenus;
     [SerializeField] GameObject _audioMenu;
+    [SerializeField] GameObject _textSettingsMenu;
 
     [Header("Parameters")]
     //[SerializeField] bool _isPaused = false;
     [SerializeField] bool _isSettingsDisplay = false;
     [SerializeField] bool _isAudioDisplay = false;
     [SerializeField] bool _isRebindDisplay = false;
+    [SerializeField] bool _isTextSettingsDisplay = false;
 
     void Start()
     {
-        _pauseMenus.SetActive(false);
-        _audioMenu.SetActive(false);
         _settingsMenus.SetActive(false);
-        _rebindMenus.SetActive(false);
     }
 
     void Update()
@@ -39,29 +38,38 @@ public class UIManager : MonoBehaviour
         _pauseMenus?.SetActive(false);
         Time.timeScale = 1f;
     }
+    public void ShowSettings()
+    {
+        if (!_isSettingsDisplay)
+        {
+            _settingsMenus?.SetActive(true);
+            _audioMenu?.SetActive(true);
+            _rebindMenus?.SetActive(false);
+        }
+        else
+        {
+            _settingsMenus?.SetActive(false);
+        }
+    }
+    public void GoBack()
+    {
+        _settingsMenus?.SetActive(false);
+        /*_audioMenu?.SetActive(false);
+        _rebindMenus?.SetActive(false);*/
+        _pauseMenus.SetActive(true);
+    }
 
     public void ShowAudioParam()
     {
         if (!_isAudioDisplay)
         {
             _audioMenu?.SetActive(true);
-        }
-        else if(!_isAudioDisplay)
-        {
-            _audioMenu?.SetActive(true);
             _rebindMenus?.SetActive(false);
-        }
-    }
-
-    public void ShowSettings()
-    {
-        if (!_isSettingsDisplay)
-        {
-            _settingsMenus?.SetActive(true);
+            _textSettingsMenu?.SetActive(false);
         }
         else
         {
-            _settingsMenus?.SetActive(false);
+            _audioMenu?.SetActive(false);
         }
     }
 
@@ -70,10 +78,26 @@ public class UIManager : MonoBehaviour
         if (!_isRebindDisplay)
         {
             _rebindMenus?.SetActive(true);
+            _audioMenu?.SetActive(false);
+            _textSettingsMenu?.SetActive(false);
         }
         else
         {
             _rebindMenus?.SetActive(false);
+        }
+    }
+
+    public void ShowTextSettings()
+    {
+        if (!_isTextSettingsDisplay)
+        {
+            _textSettingsMenu?.SetActive(true);
+            _audioMenu?.SetActive(false);
+            _rebindMenus?.SetActive(false);
+        }
+        else
+        {
+            _textSettingsMenu?.SetActive(false);
         }
     }
 }
