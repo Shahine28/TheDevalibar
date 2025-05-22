7e8fb72ced5c591147015302a78f6a9800efacd1
riufix <enguerrand.cousin@gmail.com>
2025-05-22 15:02:44 +0200

    [MOD&ADD]: Audio Manager + Audio script
    mod:
    -audio manager now functional
    
    add:
    -Audio witch can change value (pitch,etc...)

--- Assets/_TheDevalibar/GP/Audio/AudioManager.cs
+++ Assets/_TheDevalibar/GP/Audio/AudioManager.cs
@@ -1,72 +1,54 @@
+using NaughtyAttributes;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-    [Header("Audio parameters")]
-    [SerializeField] AUDIO_PROPERTIES _noCategory = new AUDIO_PROPERTIES();
-    [SerializeField] AUDIO_PROPERTIES _ambiance = new AUDIO_PROPERTIES();
-    [SerializeField] AUDIO_PROPERTIES _music = new AUDIO_PROPERTIES();
-    [SerializeField] AUDIO_PROPERTIES _SFX = new AUDIO_PROPERTIES();
-    [SerializeField] AUDIO_PROPERTIES _foley = new AUDIO_PROPERTIES();
-    [SerializeField] AUDIO_PROPERTIES _voices = new AUDIO_PROPERTIES();
-
-    [Serializable]
-    public enum AUDIO_CATEGORY
-    {
-        NONE = 0,
-        Ambiance = 10,
-        Music = 11,
-        SFX = 12,
-        Foley = 13,
-        Voices = 14
-    }
-
-    [Serializable]
-    public struct AUDIO_PROPERTIES
-    {
-        [Range(0, 1)] public float _volume;
-        [Range(-3, 3)] public float _pitch;
-        [Range(-1, 1)] public float _stereoPan;
-        [Range(0, 1)] public float _spacialBlend;
-        [Range(0, 1.1f)] public float _reverbZoneMix;
-
-        public AUDIO_PROPERTIES(bool val = true)
-        {
-            _volume = 1f;
-            _pitch = 0f;
-            _stereoPan = 0f;
-            _spacialBlend = 0f;
-            _reverbZoneMix = 0f;
-        }
-    }
-
     [Header("Audio List")]
-    [SerializeField] private List<AudioClip> _audioList;
-    [Header("Audio Sources")]
-    [SerializeField] GameObject _audioChild;
+    [SerializeField]public Audio[] _audio;
+    
+    public static AudioManager instance;
 
-    [SerializeField] private AudioSource _ambianceAudioSource;
-    [SerializeField] private AudioSource _musicAudioSource;
-    [SerializeField] private AudioSource _voiceAudioSource;
-    [SerializeField] private List<AudioSource> _audioSource = new List<AudioSource>();
-
-    public AudioClip GetAudioClip(string audioName)
+    private void Awake()
     {
-        foreach (AudioClip audio in _audioList)
+        if (instance == null)
+            instance = null;
+        else
         {
-            if(audioName == audio.name) return audio;
+            Destroy(gameObject);
+            return;
         }
-        return null;
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Audio audio in _audio)
+        {
+            audio._source = gameObject.AddComponent<AudioSource>();
+            audio._source.clip = audio._clip;
+
+            audio._source.volume = audio._volume;
+            audio._source.pitch = audio._pitch;
+            audio._source.loop = audio._loop;
+            audio._source.playOnAwake = false;
+        }
+    }
+    private void Start()
+    {
+        Play("Music");
     }
 
-    public AudioClip GetAudioClip(List<AudioClip> localAudioList, string audioName)
+    public void Play(string name)
     {
-        foreach(AudioClip audio in localAudioList)  
+        Audio a = Array.Find(_audio, audio => audio._name == name);
+        if (a == null)
         {
-            if (audioName == audio.name) return audio;
+            Debug.LogWarning("Sound:" + name + "not found");
+            return;
         }
-        return null;
+        a._source.Play();
     }
 }
