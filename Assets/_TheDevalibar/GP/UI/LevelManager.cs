f8d26fc6164d981b1c1393f4d514348b37771443
riufix <enguerrand.cousin@gmail.com>
2025-06-03 16:59:13 +0200

    [MOD]: integration of all UI asset + try fix handle problem

--- Assets/_TheDevalibar/GP/UI/LevelManager.cs
+++ Assets/_TheDevalibar/GP/UI/LevelManager.cs
@@ -7,17 +7,13 @@ public class LevelManager : MonoBehaviour
     [SerializeField] SceneAsset _GameScene;
     //[SerializeField] SceneManager _GameScene;
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
 
     public void Play()
+    {
+        SceneManager.LoadScene(_GameScene.name);
+    }
+
+    public void QuitToMain()
     {
         SceneManager.LoadScene(_GameScene.name);
     }
