f40764ef451aeee87695a2ff3ab3ed179bccac0c
riufix <enguerrand.cousin@gmail.com>
2025-06-11 10:18:54 +0200

    [MOD]: Main Menu

--- Assets/_TheDevalibar/GP/UI/UIManager.cs
+++ Assets/_TheDevalibar/GP/UI/UIManager.cs
@@ -21,17 +21,7 @@ public class UIManager : MonoBehaviour
     [Header("device container")]
     [SerializeField] GameObject _DeviceDispaly;
 
-    [Header("Parameters")]
-    //[SerializeField] bool _isPaused = false;
-    [SerializeField] bool _isSettingsDisplay = false;
-    [SerializeField] bool _isAudioDisplay = false;
-    [SerializeField] bool _isRebindDisplay = false;
-    [SerializeField] bool _isTextSettingsDisplay = false;
-    [SerializeField] bool _isKeyboardControlDisplay = false;
-    [SerializeField] bool _isMouseControlDisplay = false;
-    [SerializeField] bool _isControlerControlDisplay = false;
-    [SerializeField] bool _isDeviceDisplay = false;
-
+    
     [Header("Controller Selection")]
     [SerializeField] EventSystem _selected;
     [SerializeField] GameObject _newSelected;
@@ -47,13 +37,9 @@ public class UIManager : MonoBehaviour
     {
         if (!context.performed) return;
         if (_pauseMenus.gameObject.activeInHierarchy)
-        {
             Resume();
-        }
         else
-        {
             Pause();
-        }
     }
 
     public void Pause()
@@ -69,23 +55,16 @@ public class UIManager : MonoBehaviour
     }
     public void ShowSettings()
     {
-        if (!_isSettingsDisplay)
-        {
-            _settingsMenus?.SetActive(true);
-            _audioMenu?.SetActive(true);
-            //_mainMenu?.SetActive(false);
-            _pauseMenus.SetActive(false);
-            _rebindMenus?.SetActive(false);
-            _textSettingsMenu?.SetActive(false);
-            StartCoroutine(coucou());
-            //set the first selected obj when using controller
-           // _selected.SetSelectedGameObject(_newSelected);
-            //_selected.firstSelectedGameObject = _newSelected;
-        }
-        else
-        {
-            _settingsMenus?.SetActive(false);
-        }
+        _settingsMenus?.SetActive(true);
+        _audioMenu?.SetActive(true);
+        //_mainMenu?.SetActive(false);
+        _pauseMenus.SetActive(false);
+        _rebindMenus?.SetActive(false);
+        _textSettingsMenu?.SetActive(false);
+        StartCoroutine(coucou());
+        //set the first selected obj when using controller
+        // _selected.SetSelectedGameObject(_newSelected);
+        //_selected.firstSelectedGameObject = _newSelected;
     }
 
     IEnumerator coucou()
@@ -114,108 +93,62 @@ public class UIManager : MonoBehaviour
 
     public void ShowAudioParam()
     {
-        if (!_isAudioDisplay)
-        {
-            _audioMenu?.SetActive(true);
-            _rebindMenus?.SetActive(false);
-            _textSettingsMenu?.SetActive(false);
-            _mainMenu.SetActive(false);
-            _keyboardDisplay?.SetActive(false);
-            _mouseDisplay?.SetActive(false);
-            _controllerDisplay?.SetActive(false);
-        }
-        else
-        {
-            _audioMenu?.SetActive(false);
-        }
+        _audioMenu?.SetActive(true);
+        _rebindMenus?.SetActive(false);
+        _textSettingsMenu?.SetActive(false);
+        _mainMenu.SetActive(false);
+        _keyboardDisplay?.SetActive(false);
+        _mouseDisplay?.SetActive(false);
+        _controllerDisplay?.SetActive(false);
     }
 
     public void ShowRebinds()
     {
-        if (!_isRebindDisplay)
-        {
-            _rebindMenus?.SetActive(true);
-            _DeviceDispaly?.SetActive(true);
+        _rebindMenus?.SetActive(true);
+        _DeviceDispaly?.SetActive(true);
 
-            _audioMenu?.SetActive(false);
-            _textSettingsMenu?.SetActive(false);
-            _keyboardDisplay?.SetActive(false);
-            _mouseDisplay?.SetActive(false);
-            _controllerDisplay?.SetActive(false);
-        }
-        else
-        {
-            _rebindMenus?.SetActive(false);
-            _DeviceDispaly?.SetActive(false);
-        }
+        _audioMenu?.SetActive(false);
+        _textSettingsMenu?.SetActive(false);
+        _keyboardDisplay?.SetActive(false);
+        _mouseDisplay?.SetActive(false);
+        _controllerDisplay?.SetActive(false);
     }
 
     //---- Keyboard ----//
     public void DisplayKeyboardRebind()
     {
-        if (!_isKeyboardControlDisplay)
-        {
-            _keyboardDisplay?.SetActive(true);
-            _DeviceDispaly?.SetActive(false);
+        _keyboardDisplay?.SetActive(true);
+        _DeviceDispaly?.SetActive(false);
 
-            _controllerDisplay?.SetActive(false);
-            _mouseDisplay?.SetActive(false);
-        }
-        else
-        {
-            _keyboardDisplay?.SetActive(false);
-            _DeviceDispaly?.SetActive(true);
-        }
+        _controllerDisplay?.SetActive(false);
+        _mouseDisplay?.SetActive(false);
     }
 
     //---- Mouse ----//
     public void DisplayMouseRebind()
     {
-        if (!_isMouseControlDisplay)
-        {
-            _mouseDisplay?.SetActive(true);
-            _DeviceDispaly?.SetActive(false);
-            _keyboardDisplay?.SetActive(false);
-            _controllerDisplay?.SetActive(false);
-        }
-        else
-        {
-            _mouseDisplay?.SetActive(false);
-            _DeviceDispaly?.SetActive(true);
-        }
+        _mouseDisplay?.SetActive(true);
+        _DeviceDispaly?.SetActive(false);
+        _keyboardDisplay?.SetActive(false);
+        _controllerDisplay?.SetActive(false);
     }
 
     //---- Controller ----//
     public void DisplayControllerRebind()
     {
-        if (!_isControlerControlDisplay)
-        {
-            _controllerDisplay?.SetActive(true);
-            _DeviceDispaly?.SetActive(false);
-            _mouseDisplay?.SetActive(false);
-            _keyboardDisplay?.SetActive(false);
-        }
-        else
-        {
-            _controllerDisplay?.SetActive(false);
-            _DeviceDispaly?.SetActive(true);
-        }
+        _controllerDisplay?.SetActive(true);
+        _DeviceDispaly?.SetActive(false);
+        _mouseDisplay?.SetActive(false);
+        _keyboardDisplay?.SetActive(false);
     }
 
     public void ShowTextSettings()
     {
-        if (!_isTextSettingsDisplay)
-        {
-            _textSettingsMenu?.SetActive(true);
-            _audioMenu?.SetActive(false);
-            _rebindMenus?.SetActive(false);
-            _keyboardDisplay?.SetActive(false);
-            _mouseDisplay?.SetActive(false);
-            _controllerDisplay?.SetActive(false);
-        }
-        else
-        {
-            _textSettingsMenu?.SetActive(false);
-        }
+        _textSettingsMenu?.SetActive(true);
+        _audioMenu?.SetActive(false);
+        _rebindMenus?.SetActive(false);
+        _keyboardDisplay?.SetActive(false);
+        _mouseDisplay?.SetActive(false);
+        _controllerDisplay?.SetActive(false);
     }
 }
