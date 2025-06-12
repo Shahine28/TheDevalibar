fc7ba811e4ffadbdc9d00130f785a3c61e3c119f
Shahine Benthanane <shahine93300@gmail.com>
2025-06-12 04:21:17 +0200

    Menu presque fini

---
+++ Assets/_TheDevalibar/GP/UI/Menu/SettingsMenu.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AYellowpaper.SerializedCollections;
+using UnityEngine;
+
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializedDictionary("Menu Type", "GameObject"), SerializeField]
+    private SerializedDictionary<MenuType, GameObject> _settingsMenus = new SerializedDictionary<MenuType,  GameObject>();
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ShowSettingsMenuByType(MenuType rebindMenuType)
+    {
+        _settingsMenus[rebindMenuType]?.gameObject.SetActive(true);
+        List<MenuType> rebindMenuTypes = _settingsMenus.Keys.ToList();
+        for (int i = 0; i < rebindMenuTypes.Count; i++)
+        {
+            if (rebindMenuTypes[i] != rebindMenuType)
+            {
+                _settingsMenus[rebindMenuTypes[i]]?.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void ShowAudioMenu()
+    {
+        ShowSettingsMenuByType(MenuType.Audio);
+    }
+    
+    public void ShowRebindMenu()
+    {
+        ShowSettingsMenuByType(MenuType.Rebind);
+    }
+    
+    public void ShowTextMenu()
+    {
+        ShowSettingsMenuByType(MenuType.Text);
+    }
+
+    private void OnEnable()
+    {
+        ShowAudioMenu();
+    }
+}
+
+[Serializable]
+public enum MenuType
+{
+    Audio,
+    Rebind,
+    Text,
+}
