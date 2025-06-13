c72ebb1b219d1879a74469e2f0378a93e78e8761
Shahine Benthanane <shahine93300@gmail.com>
2025-06-13 16:00:53 +0200

    Build friendly

--- Assets/_TheDevalibar/GP/CameraMovementAndZoomControl.cs
+++ Assets/_TheDevalibar/GP/CameraMovementAndZoomControl.cs
@@ -47,7 +47,11 @@ public class CameraMovementAndZoomControl : MonoBehaviour
     }
     public void Update()
     {
-        if (!_inputValuesManager) return;
+        if (!_inputValuesManager)
+        {
+            _inputValuesManager = ServiceLocator.Get<InputValuesManager>();
+            if (!_inputValuesManager) return;
+        }
 
         if (GetZoomPercentage() > 0.2f)
         {
--- Assets/_TheDevalibar/GP/UI/Text/TextResizerManager.cs
+++ Assets/_TheDevalibar/GP/UI/Text/TextResizerManager.cs
@@ -41,6 +41,19 @@ public class TextResizerManager : MonoBehaviour
     private UnityAction<float> _worldCharacterSpacingListener;
     private UnityAction<float> _worldWordSpacingListener;
     private UnityAction<float> _worldLineSpacingListener;
+    
+   [SerializeField]private float _currentUIFontSize;
+   [SerializeField]private float _currentUICharacterSpacing;
+   [SerializeField]private float _currentUIWordSpacing;
+   [SerializeField]private float _currentUILineSpacing;
+   [SerializeField]private TMP_FontAsset _currentUIFont;
+
+   [SerializeField]private float _currentWorldFontSize;
+   [SerializeField]private float _currentWorldCharacterSpacing;
+   [SerializeField]private float _currentWorldWordSpacing;
+   [SerializeField]private float _currentWorldLineSpacing;
+   [SerializeField]private TMP_FontAsset _currentWorldFont;
+    
     private void Awake()
     {
         if (Instance == null)
@@ -58,55 +71,73 @@ public class TextResizerManager : MonoBehaviour
     void Start()
     {
         //---- UI ----//
-        _uiFontSizeListener = v => UpdateSetting((int)v, _uiFontSize, TextSettingModifier.FontSize, _cachedUIText);
+        _uiFontSizeListener = v => UpdateSetting((int)v, _uiFontSize, TextSettingModifier.FontSize, _cachedUIText,ref _currentUIFontSize);
         _uiFontSize.SettingSlider.onValueChanged.AddListener(_uiFontSizeListener);
         
-        _uiCharacterSpacingListener = v => UpdateSetting((int)v, _uiCharacterSpacing, TextSettingModifier.CharacterSpacing, _cachedUIText);
+        _uiCharacterSpacingListener = v => UpdateSetting((int)v, _uiCharacterSpacing, TextSettingModifier.CharacterSpacing, _cachedUIText, ref _currentUICharacterSpacing);
         _uiCharacterSpacing.SettingSlider.onValueChanged.AddListener(_uiCharacterSpacingListener);
         
-        _uiWordSpacingListener = v => UpdateSetting((int)v, _uiWordSpacing, TextSettingModifier.WordSpacing, _cachedUIText);
+        _uiWordSpacingListener = v => UpdateSetting((int)v, _uiWordSpacing, TextSettingModifier.WordSpacing, _cachedUIText, ref _currentUIWordSpacing);
         _uiWordSpacing.SettingSlider.onValueChanged.AddListener(_uiWordSpacingListener);
         
-        _uiLineSpacingListener = v => UpdateSetting((int)v, _uiLineSpacing, TextSettingModifier.LineSpacing, _cachedUIText);
+        _uiLineSpacingListener = v => UpdateSetting((int)v, _uiLineSpacing, TextSettingModifier.LineSpacing, _cachedUIText, ref _currentUILineSpacing);
         _uiLineSpacing.SettingSlider.onValueChanged.AddListener(_uiLineSpacingListener);
         
 
         //---- WORLD ----//
-        _worldFontSizeListener = v => UpdateSetting((int)v, _worldFontSize, TextSettingModifier.FontSize, _cachedWorldText);
+        _worldFontSizeListener = v => UpdateSetting((int)v, _worldFontSize, TextSettingModifier.FontSize, _cachedWorldText,ref _currentWorldFontSize);
         _worldFontSize.SettingSlider.onValueChanged.AddListener(_worldFontSizeListener);
         
-        _worldCharacterSpacingListener = v => UpdateSetting((int)v, _worldCharacterSpacing, TextSettingModifier.CharacterSpacing, _cachedWorldText);
+        _worldCharacterSpacingListener = v => UpdateSetting((int)v, _worldCharacterSpacing, TextSettingModifier.CharacterSpacing, _cachedWorldText, ref _currentWorldCharacterSpacing);
         _worldCharacterSpacing.SettingSlider.onValueChanged.AddListener(_worldCharacterSpacingListener);
         
-        _worldWordSpacingListener = v => UpdateSetting((int)v, _worldWordSpacing, TextSettingModifier.WordSpacing, _cachedWorldText);
+        _worldWordSpacingListener = v => UpdateSetting((int)v, _worldWordSpacing, TextSettingModifier.WordSpacing, _cachedWorldText, ref _currentWorldWordSpacing);
         _worldWordSpacing.SettingSlider.onValueChanged.AddListener(_worldWordSpacingListener);
         
-        _worldLineSpacingListener = v => UpdateSetting((int)v, _worldLineSpacing, TextSettingModifier.LineSpacing, _cachedWorldText);
+        _worldLineSpacingListener = v => UpdateSetting((int)v, _worldLineSpacing, TextSettingModifier.LineSpacing, _cachedWorldText, ref _currentWorldLineSpacing);
         _worldLineSpacing.SettingSlider.onValueChanged.AddListener(_worldLineSpacingListener);
         
+        SetDefaultValue();
         UpdateTextRegistry();
     }
 
     private void OnDestroy()
     {
         //---- UI ----//
-        _uiFontSize.SettingSlider.onValueChanged.RemoveListener(_uiFontSizeListener);
-        _uiCharacterSpacing.SettingSlider.onValueChanged.RemoveListener(_uiCharacterSpacingListener);
-        _uiWordSpacing.SettingSlider.onValueChanged.RemoveListener(_uiWordSpacingListener);
-        _uiLineSpacing.SettingSlider.onValueChanged.RemoveListener(_uiLineSpacingListener);
+        if (_uiFontSizeListener != null) _uiFontSize.SettingSlider.onValueChanged.RemoveListener(_uiFontSizeListener);
+        if (_uiCharacterSpacingListener != null) _uiCharacterSpacing.SettingSlider.onValueChanged.RemoveListener(_uiCharacterSpacingListener);
+        if (_uiWordSpacingListener != null) _uiWordSpacing.SettingSlider.onValueChanged.RemoveListener(_uiWordSpacingListener);
+        if (_uiLineSpacingListener != null) _uiLineSpacing.SettingSlider.onValueChanged.RemoveListener(_uiLineSpacingListener);
         
 
         //---- WORLD ----//
-        _worldFontSize.SettingSlider.onValueChanged.RemoveListener(_worldFontSizeListener);
-        _worldCharacterSpacing.SettingSlider.onValueChanged.RemoveListener(_worldCharacterSpacingListener);
-        _worldWordSpacing.SettingSlider.onValueChanged.RemoveListener(_worldWordSpacingListener);
-        _worldLineSpacing.SettingSlider.onValueChanged.RemoveListener(_worldLineSpacingListener);
+        if (_worldFontSizeListener != null) _worldFontSize.SettingSlider.onValueChanged.RemoveListener(_worldFontSizeListener);
+        if (_worldCharacterSpacingListener != null) _worldCharacterSpacing.SettingSlider.onValueChanged.RemoveListener(_worldCharacterSpacingListener);
+        if (_worldWordSpacingListener != null) _worldWordSpacing.SettingSlider.onValueChanged.RemoveListener(_worldWordSpacingListener);
+        if (_worldLineSpacingListener != null) _worldLineSpacing.SettingSlider.onValueChanged.RemoveListener(_worldLineSpacingListener);
     }
 
-    void UpdateSetting(int newValue,TextSettingContainer setting, TextSettingModifier settingModifier, List<TMP_Text> textList)
+    void SetDefaultValue()
+    {
+        _currentUIFontSize = _uiFontSize.SettingValue;
+        _currentUICharacterSpacing = _uiCharacterSpacing.SettingValue;
+        _currentUIWordSpacing = _uiWordSpacing.SettingValue;
+        _currentUILineSpacing = _uiLineSpacing.SettingValue;
+        _currentUIFont = _defaultFont;
+        
+        _currentWorldFontSize = _worldFontSize.SettingValue;
+        _currentWorldCharacterSpacing = _worldCharacterSpacing.SettingValue;
+        _currentWorldWordSpacing = _worldWordSpacing.SettingValue;
+        _currentWorldLineSpacing = _worldLineSpacing.SettingValue;
+        _currentWorldFont = _defaultFont;
+        
+    }
+
+    void UpdateSetting(int newValue,TextSettingContainer setting, TextSettingModifier settingModifier, List<TMP_Text> textList,ref float valueToUpdate)
     {
         setting.SettingValueText.text = newValue.ToString();
         setting.UpdateValue(newValue);
+        valueToUpdate = newValue;
         switch (settingModifier)
         {
             case TextSettingModifier.FontSize:
@@ -137,10 +168,28 @@ public class TextResizerManager : MonoBehaviour
         _cachedUIText = FindObjectsByType<TextMeshProUGUI>(FindObjectsInactive.Include, FindObjectsSortMode.None)
             .Cast<TMP_Text>()
             .ToList();
-    
+
+        foreach (var text in _cachedUIText)
+        {
+            text.fontSize = _currentUIFontSize;
+            text.characterSpacing = _currentUICharacterSpacing;
+            text.wordSpacing = _currentUIWordSpacing;
+            text.lineSpacing = _currentUILineSpacing;
+            text.font = _currentUIFont;
+        }
+        
         _cachedWorldText = FindObjectsByType<TextMeshPro>(FindObjectsInactive.Include, FindObjectsSortMode.None)
             .Cast<TMP_Text>()
             .ToList();
+        
+        foreach (var text in _cachedWorldText)
+        {
+            text.fontSize = _currentWorldFontSize;
+            text.characterSpacing = _currentWorldCharacterSpacing;
+            text.wordSpacing = _currentWorldWordSpacing;
+            text.lineSpacing = _currentWorldLineSpacing;
+            text.font = _currentWorldFont;
+        }
     }
 
     #region Button
@@ -172,6 +221,9 @@ public class TextResizerManager : MonoBehaviour
         {
             text.font = newFont;
         }
+        
+        _currentUIFont = newFont;
+        _currentWorldFont = newFont;
     }
     #endregion
 }
